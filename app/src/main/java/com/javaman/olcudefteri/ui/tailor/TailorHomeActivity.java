4e297b327757790fe2d304452d0ba6012d4d8e7a
snfull <snfullstack@gmail.com>
2018-06-07 18:39:02 +0300

    last changes

--- app/src/main/java/com/javaman/olcudefteri/ui/tailor/TailorHomeActivity.java
+++ app/src/main/java/com/javaman/olcudefteri/ui/tailor/TailorHomeActivity.java
@@ -393,7 +393,7 @@ public class TailorHomeActivity extends AppCompatActivity implements BaseView,Ta
 
     @Override
     public void setLogoutPref(boolean isLogout) {
-
+        sharedPreferenceHelper.setBooleanPreference("logout",isLogout);
     }
 
     @Override
